56faaf3a03a15e8dcd166a7f8029a5253a7c14fe
unknown <charles.pitek@gmail.com>
2020-01-10 14:42:16 -0800

    Cleaned up the page clear code

--- script.js
+++ script.js
@@ -4,10 +4,7 @@
 
 //This function will change the page from the intro page to the plan trip page
 function planNowPageChange () {
-    $("#welcomeJumbotron").css({
-        "display":"none"
-    })
-    $("#welcomeCards").css({
+    $("#welcome").css({
         "display":"none"
     })
 }

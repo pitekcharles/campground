dea0e77b7c3e542be2110c71357edd44891e396d
unknown <charles.pitek@gmail.com>
2020-01-10 15:18:37 -0800

    got state select drop down populated

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //variable creation
+campgroundApiKey = "b8ux8fnrgv79fk8thpzh3dk5";
+
+
 
 //function creation
 
@@ -8,7 +11,19 @@ function planNowPageChange () {
         "display":"none"
     })
 }
-//function call outs
 
+//This function will populate the drop down for state select with all state names
+//from the stateAry
+function statePop () {
+    stateAry.forEach(function(item){
+        var newOption = $("<option>").attr({
+            "data-state": item
+        });
+        newOption.text(item);
+        $("#stateName").append(newOption);
+    })
+}
+//function call outs
+statePop();
 //on-click events
 $("#planNowButton").on("click", planNowPageChange);
